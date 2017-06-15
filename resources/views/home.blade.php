b90cfbf4d76a4b2a065d83b0cd41114bf0f79331
Lu <lucien.brunet@gmail.com>
2017-06-15 03:35:38 -0400

    added form to add topics to home screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,14 @@
             <div class="row">
                 <div class="col-sm-4 push-8">
                     <h2>Topics</h2>
-                    <p><a href=# id="add-topic">Add a topic</a></p>
+                    <p><a href=# id="toggle-add-topic">Add a topic</a></p>
+                    <form id="add-topic">
+                        <div class="form-group">
+                            <label for="new-topic-name">New Topic</label>
+                            <input type="text" id="new-topic-name">
+                            <button class="btn btn-primary">Add</button>
+                        </div>
+                    </form>
                 </div>
                 <div class="col-sm-8 pull-4">
                     <h1>Welcome!</h1>
@@ -17,4 +24,23 @@
         </section>
     </article>
 </div>
+
+
+<script>
+
+$(function (){
+    $('#add-topic').hide();
+    $('#toggle-add-topic').on('click', function(e) {
+        e.preventDefault();
+        $('#add-topic').toggle();
+    });
+    $('#add-topic').on('submit', function(e) {
+        e.preventDefault();
+        var topic_name = $('#add-topic input').val();
+        //send ajax call to controller.
+        $('#add-topic input').val('');
+    });
+});
+
+</script>
 @endsection
